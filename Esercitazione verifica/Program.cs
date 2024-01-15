3d0720e14898daf5327e228a70e613ec8d646212
Rota.20073 <rotaale05102006@gmail.com>
2024-01-15 09:58:24 +0100

    Completo ma non funzionante del tutto

--- Esercitazione verifica/Program.cs
+++ Esercitazione verifica/Program.cs
@@ -49,9 +49,11 @@ namespace Esercitazione_verifica
         {
             get => saldo;
             set
-            { if (value > 0)
+            { 
+                if (value > 0)
                     saldo = value;
-                else Console.WriteLine("Inserire un valore maggiore di 0");
+                else 
+                    Console.WriteLine("Inserire un valore maggiore di 0");
             }
         }
         private bool chiuso;
@@ -93,7 +95,7 @@ namespace Esercitazione_verifica
         }
         public void getinfo()
         {
-            Console.WriteLine("Nome e cognome del titolare del conto corrente:" + Nome + " " + "dalla provincia di: " + Provincia);
+            Console.WriteLine("Nome e cognome del titolare del conto corrente:" + Nome + " " + Cognome + " "+ "dalla provincia di: " + Provincia);
             if (chiuso) { Console.WriteLine("Conto aperto"); }
             else { Console.WriteLine("Conto chiuso"); }
             Console.WriteLine("Saldo all'interno del conto: "+ saldo);
@@ -114,13 +116,19 @@ namespace Esercitazione_verifica
         {
             arrayconti[i].apri();
             i++;
-
-            Console.WriteLine("Benvenuto nella banca!");
+            Console.WriteLine("Benvenuto nella banca!\n");
+            Console.WriteLine("Inserisci il nome");
+            arrayconti[i].Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il cognome");
+            arrayconti[i].Cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci la provincia");
+            arrayconti[i].Provincia = Console.ReadLine();
         }
         public void Chiudiconto()
         {
             arrayconti[i].chiudiconto();
             i++;
+            Console.WriteLine("Il conto è stato chiuso.\n");
         }
         public void Depositasuconto()
         {
@@ -134,7 +142,7 @@ namespace Esercitazione_verifica
         }
         public void Vedisaldoconto()
         {
-            Console.WriteLine("Il saldo sul conto è di: "+ arrayconti[i].ssaldo());
+            Console.WriteLine("Il saldo sul conto è di: " + arrayconti[i].ssaldo() + "\n");
         }
         public void Vediinfoconto()
         {
@@ -148,24 +156,59 @@ namespace Esercitazione_verifica
             banca paolobanchero = new banca();
             string risposta;
 
-            Console.WriteLine("Inserisci il numero corrispondente alla pratica da svolgere:");
-            Console.WriteLine("1) Apri il conto");
-            Console.WriteLine("2) Chiudi il conto"); 
-            Console.WriteLine("3) Deposita sul conto");
-            Console.WriteLine("4) Preleva dal conto");
-            Console.WriteLine("5) Vedi saldo sul conto");
-            Console.WriteLine("6) Vedi informazioni sul conto");
-            risposta = Console.ReadLine();
-
-            switch (risposta)
+            do
             {
-                case "1":
+                Console.WriteLine("Inserisci il numero corrispondente alla pratica da svolgere:");
+                Console.WriteLine("1) Apri il conto");
+                Console.WriteLine("2) Chiudi il conto");
+                Console.WriteLine("3) Deposita sul conto");
+                Console.WriteLine("4) Preleva dal conto");
+                Console.WriteLine("5) Vedi saldo sul conto");
+                Console.WriteLine("6) Vedi informazioni sul conto");
+                Console.WriteLine("7) Esci");
 
-                    paolobanchero.Apriconto();
+                risposta = Console.ReadLine();
 
+                switch (risposta)
+                {
+                    case "1":
 
-                    break;
-            }
+                        Console.Clear();
+                        paolobanchero.Apriconto();
+                        break;
+
+                    case "2":
+
+                        Console.Clear();
+                        paolobanchero.Chiudiconto();
+                        break;
+
+                    case "3":
+
+                        Console.Clear();
+                        paolobanchero.Depositasuconto();
+                        break;
+
+                    case "4":
+
+                        Console.Clear();
+                        paolobanchero.Prelevadaconto();
+                        break;
+
+                    case "5":
+
+                        Console.Clear();
+                        paolobanchero.Vedisaldoconto();
+                        break;
+
+                    case "6":
+
+                        Console.Clear();
+                        paolobanchero.Vediinfoconto();
+                        break;
+                }
+            } while (risposta != string.Empty || risposta != "7");
+
             Console.ReadKey();
         }
     }
